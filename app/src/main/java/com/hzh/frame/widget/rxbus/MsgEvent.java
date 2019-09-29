1659d30f3571b9a74679884d80f71c37e8378c88
153200037@qq.com <153200037@qq.com>
2019-09-29 13:44:09 +0800

    hzh

--- app/src/main/java/com/hzh/frame/widget/rxbus/MsgEvent.java
+++ app/src/main/java/com/hzh/frame/widget/rxbus/MsgEvent.java
@@ -1,22 +1,46 @@
 package com.hzh.frame.widget.rxbus;
 
+import com.google.gson.Gson;
+
 /**
  * @author
  * @version 1.0
  * @date 2019/2/24
  */
-public class MsgEvent {
-    private String msg;
+public class MsgEvent<T> {
 
-    public MsgEvent(String msg) {
+    private T msg;
+    private String tag;
+
+    public MsgEvent(T msg) {
         this.msg = msg;
+        this.tag = "mainLine";
     }
 
-    public String getMsg() {
+    public MsgEvent(String tag,T msg) {
+        this.msg = msg;
+        this.tag = tag;
+    }
+
+    public T getMsg() {
         return msg;
     }
 
-    public void setMsg(String msg) {
+    public MsgEvent setMsg(T msg) {
         this.msg = msg;
+        return this;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public MsgEvent setTag(String tag) {
+        this.tag = tag;
+        return this;
+    }
+
+    public String toString(){
+        return new Gson().toJson(this);
     }
 }
